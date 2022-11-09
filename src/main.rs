a889fb3f60d217f423c69b4b1931ddb06f9a8ee1
Davide Forlani <davide.forlani@overweb.it>
2022-11-09 21:36:10 +0100

    Reformatted.

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,23 @@ use std::fs::File;
 // to spawn a new process
 use std::process::Command;
 // Local
-use menu::{get_conf_dir,ask,term};
+use menu::{ask, get_conf_dir, term};
 // External
 use serde::Deserialize;
 use serde_json as json;
 
 fn main() {
-    set_current_dir( get_menu_dir() ).unwrap();
+    set_current_dir(get_menu_dir()).unwrap();
     menu();
 }
 
 fn menu() {
     let items = load();
-    let s = ask( items.iter().map(|x| x.name()) )
-        .unwrap();
+    let s = ask(items.iter().map(|x| x.name())).unwrap();
     for i in items {
         if i.name() == &s {
             i.exec();
-            break
+            break;
         }
     }
 }
@@ -28,11 +27,11 @@ fn menu() {
 fn load() -> Vec<MenuItem> {
     let r = match File::open("config.json") {
         Err(why) => term!(why),
-        Ok(r) => r
+        Ok(r) => r,
     };
     match json::from_reader(r) {
         Err(why) => term!(why),
-        Ok(j) => j
+        Ok(j) => j,
     }
 }
 
@@ -40,28 +39,24 @@ fn load() -> Vec<MenuItem> {
 #[serde(untagged)]
 enum MenuItem {
     Cmd { name: String, cmd: String },
-    Dir { name: String, dir: String }
+    Dir { name: String, dir: String },
 }
 impl MenuItem {
     fn name(&self) -> &str {
         use MenuItem::*;
         match &self {
             Cmd { name: n, .. } => &n,
-            Dir { name: n, .. } => &n
+            Dir { name: n, .. } => &n,
         }
     }
     fn exec(&self) {
         use MenuItem::*;
         match &self {
             Cmd { cmd: c, .. } => {
-                Command::new("/bin/sh")
-                    .args( [ "-c", &c ] )
-                    .spawn()
-                    .unwrap();
-            },
+                Command::new("/bin/sh").args(["-c", &c]).spawn().unwrap();
+            }
             Dir { dir: d, .. } => {
-                set_current_dir(&d)
-                    .unwrap();
+                set_current_dir(&d).unwrap();
                 menu()
             }
         }
